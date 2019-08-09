b57faeb0d4b51ea34d77b1bdada23d9d2b545e47
ignaciocarlucho <ignacio.carlucho@gmail.com>
2019-08-09 15:48:42 -0300

    remove save obs in info

--- stable_baselines/common/vec_env/dummy_vec_env.py
+++ stable_baselines/common/vec_env/dummy_vec_env.py
@@ -40,7 +40,7 @@ class DummyVecEnv(VecEnv):
                 self.envs[env_idx].step(self.actions[env_idx])
             if self.buf_dones[env_idx]:
                 # save final observation where user can get it, then reset
-                self.buf_infos[env_idx]['terminal_observation'] = obs
+                # self.buf_infos[env_idx]['terminal_observation'] = obs
                 obs = self.envs[env_idx].reset()
             self._save_obs(env_idx, obs)
         return (self._obs_from_buf(), np.copy(self.buf_rews), np.copy(self.buf_dones),
